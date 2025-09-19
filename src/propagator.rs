33638a6be62e15a43f0deed341bc92613419177f
Franco D'Ilario <franco.dilario24@gmail.com>
2025-09-19 08:59:33 +0200

    Modified PropagationResult to contain vector of positions

--- src/propagator.rs
+++ src/propagator.rs
@@ -6,7 +6,7 @@ use crate::tle::TLE;
 #[derive(Debug)]
 pub struct PropagationResult {
     pub object_name: String,
-    pub predictions: Vec<Prediction>
+    pub positions: Vec<[f64; 3]>
 }
 
 // Propagate each fetched TLE and return a vector of results.
@@ -18,15 +18,20 @@ pub fn propagate_tles(tle_list: Vec<TLE>) -> Result<Vec<PropagationResult>> {
         let elements = sgp4::Elements::from_tle(tle.name.clone(), tle.line1.as_bytes(), tle.line2.as_bytes())?;
         let constants = sgp4::Constants::from_elements(&elements)?;
         let mut predictions: Vec<Prediction> = Vec::new();
+        let mut positions: Vec<[f64; 3]> = Vec::new();
 
         for minutes in 0..1440 {
             let t = minutes as f64;
             predictions.push(constants.propagate(sgp4::MinutesSinceEpoch(t))?);
         }
 
+        for prediction in predictions {
+            positions.push(prediction.position);
+        }
+
         results.push(PropagationResult { 
             object_name: tle.name.unwrap_or_else(|| "Unnamed object".to_owned()), 
-            predictions: predictions 
+            positions: positions 
         });
     }
 
